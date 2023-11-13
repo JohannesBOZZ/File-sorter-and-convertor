a271c77b53aa1419c79fcc57ba3711de58400fbd
JohannesBOZZ <maxb12032005@gmail.com>
2023-11-13 19:45:02 +0100

    added scaling

--- File sorter and convertor.py
+++ File sorter and convertor.py
@@ -20,19 +20,29 @@ root.resizable(False,False)
 def select_mode():
     mode = f'{option_menu_sort.get()} {option_menu_convert.get()}'
     os.makedirs(dst.get(), exist_ok=True)
+
     try:
+        if '' not in [width_entry.get(), hight_entry.get() ]:
+            scale = (int(width_entry.get()), int(hight_entry.get()))
+        else:
+            scale = None
+            
         if mode == 'Date None' or mode == 'Name None' or mode == 'Date revers None' or mode == 'Name revers None':
             date_or_Name_sorter(src.get(), dst.get())
         elif mode == 'Date JPG' or mode == 'Name JPG' or mode == 'Date revers JPG' or mode == 'Name revers JPG':
-            date_or_name_sorter_jpg(src.get(), dst.get(), int(jpg_quality.get()))
+            date_or_name_sorter_jpg(src.get(), dst.get(), int(jpg_quality.get()), scale)
         elif mode == 'None JPG':
-            convert_to_jpg(src.get(), dst.get(), int(jpg_quality.get()))
+            convert_to_jpg(src.get(), dst.get(), int(jpg_quality.get()), scale)
         elif mode == 'Date PNG' or mode == 'Name PNG' or mode == 'Date revers PNG' or mode == 'Name revers PNG':
-            date_or_Name_sorter_png(src.get(), dst.get())
+            date_or_Name_sorter_png(src.get(), dst.get(), scale)
         elif mode == 'None PNG':
-            convert_to_png(src.get(), dst.get())
+            convert_to_png(src.get(), dst.get(), scale)
+        elif mode == 'Date WEBP' or mode == 'Name WEBP' or mode == 'Date revers WEBP' or mode == 'Name revers WEBP':
+            date_or_Name_sorter_webp(src.get(), dst.get(), scale, int(jpg_quality.get()))
+        elif mode == 'None WEBP':
+            convert_to_webp(src.get(), dst.get(), scale, int(jpg_quality.get()))
         elif mode == 'None None':
-            only_copy(src.get(), dst.get())
+            only_copy(src.get(), dst.get(), scale)
 
 
 
@@ -85,7 +95,7 @@ def date_or_Name_sorter(source_folder, destination_folder):
 
 
 
-def date_or_name_sorter_jpg(source_folder, destination_folder, quality):
+def date_or_name_sorter_jpg(source_folder, destination_folder, quality, scale):
     start_time = time.time()
     mode = f'{option_menu_sort.get()} {option_menu_convert.get()}'
     wb = chackbox_wb.get()
@@ -109,7 +119,7 @@ def date_or_name_sorter_jpg(source_folder, destination_folder, quality):
         for i, file in enumerate(sorted_files, start=1):
             source_path = os.path.join(root, file)
             filename, file_extension = os.path.splitext(source_path)
-            if file_extension.endswith((".png", ".jpg", ".jpeg")):
+            if file_extension.endswith((".png", ".jpg", ".jpeg", ".webp")):
                 destination_path = os.path.join(destination_folder, f"{count}.jpg")
 
                 # Checks whether checkbox wb is selected or not
@@ -117,6 +127,8 @@ def date_or_name_sorter_jpg(source_folder, destination_folder, quality):
                     img = Image.open(source_path).convert('L')
                 else:
                     img = Image.open(source_path).convert("RGB")
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)
                 img.save(destination_path, "JPEG", quality=quality)
             else:    
                 destination_path = os.path.join(destination_folder, f"{count}{file_extension}")
@@ -133,7 +145,7 @@ def date_or_name_sorter_jpg(source_folder, destination_folder, quality):
     consol_message(end_time, start_time, convertet_image_count)
 
 
-def convert_to_jpg(input_folder, output_folder, quality):
+def convert_to_jpg(input_folder, output_folder, quality, scale):
     start_time = time.time()
     convertet_image_count = 0
     wb = chackbox_wb.get()
@@ -149,6 +161,8 @@ def convert_to_jpg(input_folder, output_folder, quality):
                     img = Image.open(input_path).convert('L')
                 else:
                     img = Image.open(input_path).convert("RGB") 
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)                    
                 img.save(output_path, "JPEG", quality=quality)
                 convertet_image_count += 1
                 progress_bar_def(convertet_image_count)
@@ -162,7 +176,7 @@ def convert_to_jpg(input_folder, output_folder, quality):
     consol_message(end_time, start_time, convertet_image_count)
 
 
-def date_or_Name_sorter_png(source_folder, destination_folder):
+def date_or_Name_sorter_png(source_folder, destination_folder, scale):
     start_time = time.time()
     mode = f'{option_menu_sort.get()} {option_menu_convert.get()}'
     wb = chackbox_wb.get()
@@ -186,12 +200,16 @@ def date_or_Name_sorter_png(source_folder, destination_folder):
         for i, file in enumerate(sorted_files, start=1):
             source_path = os.path.join(root, file)
             filename, file_extension = os.path.splitext(source_path)
-            if file_extension.endswith((".png", ".jpg", ".jpeg")):
+            if file_extension.endswith((".png", ".jpg", ".jpeg", ".webp")):
                 # Checks whether checkbox wb is selected or not
                 if wb:
                     img = Image.open(source_path).convert('L')
                 else:
                     img = Image.open(source_path)
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)
+                    if wb:
+                        img = Image.convert('L')
                 png_path = os.path.join(destination_folder, f"{count}.png")
                 img.save(png_path, 'PNG')
             else:
@@ -206,14 +224,14 @@ def date_or_Name_sorter_png(source_folder, destination_folder):
     consol_message(end_time, start_time, convertet_image_count)
 
 
-def convert_to_png(input_folder, output_folder):
+def convert_to_png(input_folder, output_folder, scale):
     start_time = time.time()
     image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']  # Unterstützte Bildformate
     wb = chackbox_wb.get()
     convertet_image_count = 0
 
     for filename in os.listdir(input_folder):
-            if filename.endswith((".png", ".jpg", ".jpeg")):
+            if filename.endswith((".png", ".jpg", ".jpeg", ".webp")):
                 input_path = os.path.join(input_folder, filename)
                 output_path = os.path.join(output_folder, f'{filename}.png')
                 img=None
@@ -222,11 +240,86 @@ def convert_to_png(input_folder, output_folder):
                     img = Image.open(input_path).convert('L')
                 else:
                     img = Image.open(input_path)
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)
                 img.save(output_path, 'PNG')
                 convertet_image_count += 1
                 progress_bar_def(convertet_image_count)
 
+    end_time = time.time()
+    # calculates the time taken
+    consol_message(end_time, start_time, convertet_image_count)
 
+
+def date_or_Name_sorter_webp(source_folder, destination_folder, scale, quality):
+    start_time = time.time()
+    mode = f'{option_menu_sort.get()} {option_menu_convert.get()}'
+    wb = chackbox_wb.get()
+
+    # Create the destination folder if it doesn't exist
+    count = len([name for name in os.listdir(destination_folder) if os.path.isfile(os.path.join(destination_folder, name))])
+    convertet_image_count = 0
+    # Sort files by modified date
+    for root, _, files in os.walk(source_folder):
+        # Sortiere die Dateien nach dem Änderungsdatum
+        if mode == 'Date PNG':
+            sorted_files = sorted(files, key=lambda x: os.path.getmtime(os.path.join(root, x)))
+        elif mode == 'Name PNG':
+            sorted_files = sorted(files, key=natural_sort_key)
+        elif mode == 'Date revers PNG':
+            sorted_files = sorted(files, key=lambda x: os.path.getmtime(os.path.join(root, x)), reverse=True)
+        elif mode == 'Name revers PNG':
+            sorted_files = sorted(files, key=natural_sort_key, reverse=True)
+
+    # Copy the files to the destination folder and number them
+        for i, file in enumerate(sorted_files, start=1):
+            source_path = os.path.join(root, file)
+            filename, file_extension = os.path.splitext(source_path)
+            if file_extension.endswith((".png", ".jpg", ".jpeg", ".webp")):
+                # Checks whether checkbox wb is selected or not
+                if wb:
+                    img = Image.open(source_path).convert('L')
+                else:
+                    img = Image.open(source_path)
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)
+                    if wb:
+                        img = Image.convert('L')
+                file_path = os.path.join(destination_folder, f"{count}.webp")
+                img.save(file_path, 'WEBP', optimize = True, quality = quality)
+            else:
+                destination_path = os.path.join(destination_folder, f"{count}{file_extension}")
+                shutil.copy2(source_path, destination_path)
+            count += 1
+            convertet_image_count += 1
+            progress_bar_def(convertet_image_count)
+    # ends counting time
+    end_time = time.time()
+    # calculates the time taken
+    consol_message(end_time, start_time, convertet_image_count)
+
+
+def convert_to_webp(input_folder, output_folder, scale, quality):
+    start_time = time.time()
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']  # Unterstützte Bildformate
+    wb = chackbox_wb.get()
+    convertet_image_count = 0
+
+    for filename in os.listdir(input_folder):
+            if filename.endswith((".png", ".jpg", ".jpeg", ".webp")):
+                input_path = os.path.join(input_folder, filename)
+                output_path = os.path.join(output_folder, f'{filename}.webp')
+                img=None
+                # Versuche, das Bild zu öffnen und in PNG zu konvertieren
+                if wb:
+                    img = Image.open(input_path).convert('L')
+                else:
+                    img = Image.open(input_path)
+                if scale is not None:
+                    img.thumbnail(scale, Image.LANCZOS)
+                img.save(output_path, 'WEBP', optimize = True, quality = quality)
+                convertet_image_count += 1
+                progress_bar_def(convertet_image_count)
 
     end_time = time.time()
     # calculates the time taken
@@ -264,7 +357,7 @@ def natural_sort_key(s):
 def mode(self):
     mode = f'{option_menu_sort.get()} {option_menu_convert.get()}'
     # shows jpg_quality and chackbox_wb
-    if 'JPG' in mode and 'PNG' not in mode:
+    if 'JPG' in mode or 'WEBP' in mode and 'PNG' not in mode:
         jpg_quality.place(relx=0.64, rely=0.49, anchor=tkinter.CENTER)
         chackbox_wb.place(relx=0.86, rely=0.49, anchor=tkinter.CENTER)
     # shows chackbox_wb and hidersjpg_quality
@@ -275,6 +368,17 @@ def mode(self):
     elif option_menu_convert.get() == 'None':
         chackbox_wb.place_forget()
         jpg_quality.place_forget()
+    
+    if 'None' in option_menu_convert.get():
+        width_entry.place_forget()
+        hight_entry.place_forget()
+        src.configure(width=820)
+        dst.configure(width=820)
+    else:
+        width_entry.place(relx=0.98, rely=0.2, anchor=tkinter.E)
+        hight_entry.place(relx=0.98, rely=0.35, anchor=tkinter.E)
+        src.configure(width=700)
+        dst.configure(width=700)
 
 
 def consol_message(end_time, start_time, convertet_image_count):
@@ -321,18 +425,34 @@ label.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
 src = ctk.CTkEntry(master=frame,
                             placeholder_text='Input folder',
                             font=("sora", 20),
-                            width=825,
+                            width=700,
                             height=32,
                             corner_radius=5)
-src.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
+src.place(relx=0.02, rely=0.2, anchor=tkinter.W)
 # destination entry
 dst = ctk.CTkEntry(master=frame,
                             placeholder_text='Output folder',
                             font=("sora", 20),
-                            width=825,
+                            width=700,
                             height=32,
                             corner_radius=5)
-dst.place(relx=0.5, rely=0.35, anchor=tkinter.CENTER)
+dst.place(relx=0.02, rely=0.35, anchor=tkinter.W)
+# width entry
+width_entry = ctk.CTkEntry(master=frame,
+                            placeholder_text='width',
+                            font=("sora", 20),
+                            width=100,
+                            height=32,
+                            corner_radius=5)
+width_entry.place(relx=0.98, rely=0.2, anchor=tkinter.E)
+# hight entry
+hight_entry = ctk.CTkEntry(master=frame,
+                            placeholder_text='hight',
+                            font=("sora", 20),
+                            width=100,
+                            height=32,
+                            corner_radius=5)
+hight_entry.place(relx=0.98, rely=0.35, anchor=tkinter.E)
 # entry for quality by jpg
 jpg_quality = ctk.CTkEntry(master=frame,
                             placeholder_text='JPG Quality',
@@ -356,7 +476,7 @@ option_menu_convert = ctk.CTkOptionMenu(master=frame,
                                 width=100,
                                 height=36,
                                 corner_radius=5,
-                                values=['PNG', 'JPG', 'None'],
+                                values=['PNG', 'JPG','WEBP' , 'None'],
                                 command=mode,)
 option_menu_convert.place(relx=0.452, rely=0.49, anchor=tkinter.CENTER)
 # if you wont AI generated images
